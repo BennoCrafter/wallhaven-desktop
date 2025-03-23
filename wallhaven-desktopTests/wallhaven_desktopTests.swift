2734cc2747c75ea1f4a2cb5432a550ecd4ad987f
Benno <107504783+BennoCrafter@users.noreply.github.com>
2025-03-23 17:43:47 +0100

    init

---
+++ wallhaven-desktopTests/wallhaven_desktopTests.swift
@@ -0,0 +1,8 @@
+import Testing
+@testable import wallhaven_desktop
+
+struct wallhaven_desktopTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
